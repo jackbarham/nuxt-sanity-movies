938d89b250c805fa5f3f8e0dadcdc5b3fa831d8e
Jack Barham <>
2025-12-18 18:41:29 +0000

    Page width

--- frontend/app/layouts/default.vue
+++ frontend/app/layouts/default.vue
@@ -2,7 +2,7 @@
   <div class="flex flex-col h-screen">
     <MainHeader />
     <main role="main" class="flex-1 pt-16 md:pt-18 relative layout-wide">
-      <div class="relative min-h-full">
+      <div class="relative min-h-full py-16">
         <slot />
       </div>
     </main>
