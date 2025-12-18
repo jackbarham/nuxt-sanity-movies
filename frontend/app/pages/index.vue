bf8e85e13dff51886fde946d1a2bf7613fb6c1ba
Jack Barham <>
2025-12-18 16:39:36 +0000

    Basic page structure

---
+++ frontend/app/pages/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Movies</h1>
+    <ul>
+      <li v-for="movie in movies" :key="movie.title">
+        {{ movie.title }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script setup lang="ts">
+type Movie = {
+  title: string
+}
+
+const query = groq`*[_type == "movie"]{title}`
+const { data: movies } = await useSanityQuery<Movie[]>(query)
+</script>
